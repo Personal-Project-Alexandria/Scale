97da63eec5fb166a7b531bfead21bf47463d9aba
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2017-07-17 15:48:20 +0700

    - update UI

---
+++ Scale/Assets/Script/UI/GUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoSingleton<GUIManager> {
+
+    private List<BaseDialog> baseDialogs;
+    public Transform transfDialog;
+    void Awake()
+    {
+        this.baseDialogs = new List<BaseDialog>();
+    }
+    public T OnShowDialog<T>(string path, object data = null) where T : BaseDialog
+    {
+        T target = ((GameObject)Instantiate(Resources.Load(path))).GetComponent<T>();
+        if (target != null)
+        {
+            target.gameObject.SetActive(true);
+            target.OnShow(this.transfDialog, data);
+            this.baseDialogs.Add(target);
+        }
+        return null;
+    }
+    public void OnHideDialog(BaseDialog dialog)
+    {
+        dialog.OnHide();
+        if (this.baseDialogs.Contains(dialog))
+        {
+            this.baseDialogs.Remove(dialog);
+        }
+    }
+    public void OnHideAllDialog()
+    {
+        foreach (BaseDialog dialog in this.baseDialogs)
+        {
+            if (dialog != null)
+                dialog.OnHide();
+        }
+    }
+}
