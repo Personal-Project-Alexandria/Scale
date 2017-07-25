b10207b820f9450e7f0f8b5506ab4baed275f90d
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2017-07-25 20:54:31 +0700

    - fix link rate

--- Scale/Assets/Script/UI/RateDialog.cs
+++ Scale/Assets/Script/UI/RateDialog.cs
@@ -7,7 +7,7 @@ public class RateDialog : BaseDialog {
 	public void OnClickRate()
     {
 #if UNITY_ANDROID
-        Application.OpenURL("https://play.google.com/store/apps/details?id=com.quoclv.bestgame.scalecolor");
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.puzzlegame.freegame.scaleslicer");
 #elif UNITY_IOS
         Application.OpenURL("https://itunes.apple.com/us/app/scale-color/id1261258088");
 #endif
